974a38462921747af2b02a0be53ae17f6a0f1a09
masoud8840 <masoudgharedaghi@yahoo.com>
2022-10-22 16:24:20 +0330

    added subheader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
 <template>
-  <the-navigation></the-navigation>
   <router-view></router-view>
 </template>
-
-<script setup>
-import TheNavigation from "./components/layout/TheNavigation.vue";
-</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>This is home route</h1>
+  <the-navigation></the-navigation>
+  <section class="subheader container">
+    <article v-for="item in subheaderItems">
+      <img :src="item.imgUrl" :alt="item.title" />
+      <h4>{{ item.title }}</h4>
+      <p>{{ item.subtitle }}</p>
+    </article>
+  </section>
 </template>
+
+<script setup>
+import TheNavigation from "../components/layout/TheNavigation.vue";
+import { ref } from "vue";
+const subheaderItems = ref([
+  {
+    imgUrl: "/images/Reliable.svg",
+    title: "Reliable",
+    subtitle: "Trusted by millions of users",
+  },
+  {
+    imgUrl: "/images/Express.svg",
+    title: "Express",
+    subtitle: "1-Day arrival time",
+  },
+  {
+    imgUrl: "/images/Guarantee.svg",
+    title: "Guarantee",
+    subtitle: "8 Days guarantee",
+  },
+]);
+</script>
